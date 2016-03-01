24cbfbdb42b2177dcf78a982738459e77cdeafcd
scottenand <sae5201@psu.edu>
2016-03-01 15:58:50 -0500

    Things work!

--- cmpsc431w/setup/setup.php
+++ cmpsc431w/setup/setup.php
@@ -7,8 +7,19 @@ createTables($CLASSES);
 echo("\n\n\n\n\n\n\n");
 
 $tables = array();
-foreach($CLASSES as $c) {
-	$tables = array_merge($tables, $c::create_table());
+foreach($CLASSES as $i => $c) {
+	if(is_array($c)) {
+		foreach($c as $ci => $cn) {
+			if(is_array($cn)) {
+				foreach($cn as $cnn) {
+					if(is_string($cnn))
+						$tables = array_merge($tables, $cnn::create_table());
+				}
+			} elseif(is_string($cn))
+				$tables = array_merge($tables, $cn::create_table());
+		}
+	} elseif(is_string($c))
+		$tables = array_merge($tables, $c::create_table());
 }
 echo(implode("\n\n", $tables));
 
