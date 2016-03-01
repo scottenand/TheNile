bc5be48e6281351f00bd8de6a2b5f59a00a4e5a5
scottenand <sae5201@psu.edu>
2016-03-01 14:58:22 -0500

    Added files via upload

--- cmpsc431w/setup/setup.php
+++ cmpsc431w/setup/setup.php
@@ -1,6 +1,15 @@
 <?php
 
-include("createDB.php");
-include("createConnect.php");
+include("../Header.php");
+
+createTables($CLASSES);
+
+echo("\n\n\n\n\n\n\n");
+
+$tables = array();
+foreach($CLASSES as $c) {
+	$tables = array_merge($tables, $c::create_table());
+}
+echo(implode("\n\n", $tables));
 
 ?>
