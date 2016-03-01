4fe6f4ff930d7b83d07c0739e10ae494dcf5c5c8
scottenand <sae5201@psu.edu>
2016-03-01 14:58:42 -0500

    Added files via upload

---
+++ cmpsc431w/Header.php
@@ -0,0 +1,32 @@
+<?php
+
+include("classes/table.php");
+
+$CLASSES = array(
+	"User",
+		"Person",
+		"Company",
+
+		"Address",
+		"Phone",
+		"Creditcard",
+		"UserRating",
+
+
+	"Product",
+		"Purchase",
+		"Auction",
+
+		"Category",
+			"PartOf",
+			"ParentCategory",
+		"RatedBy",
+		"Acquired",
+			"PurchasedBy"
+);
+
+foreach($CLASSES as $c) {
+	require_once("classes/"$c . ".php");
+}
+
+?>
