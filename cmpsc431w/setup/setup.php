c506438d09062bad3d40a5c4963e99d8e4592cff
scottenand <sae5201@psu.edu>
2016-03-02 17:10:33 -0500

    Some changes to relationships

--- cmpsc431w/setup/setup.php
+++ cmpsc431w/setup/setup.php
@@ -7,20 +7,36 @@ createTables($CLASSES);
 echo("\n\n\n\n\n\n\n");
 
 $tables = array();
+$tableAttrs = array();
+
 foreach($CLASSES as $i => $c) {
 	if(is_array($c)) {
+		if($i != "Entity") {
+			$tableAttrs[] = $i . "(" . implode(", ", $i::getAttributeList()) . ")";
+			$tables = array_merge($tables, $i::create_table());
+		}
 		foreach($c as $ci => $cn) {
 			if(is_array($cn)) {
+				$tableAttrs[] = $ci . "(" . implode(", ", $ci::getAttributeList()) . ")";
+				$tables = array_merge($tables, $ci::create_table());
 				foreach($cn as $cnn) {
-					if(is_string($cnn))
+					if(is_string($cnn)) {
+						$tableAttrs[] = $cnn . "(" . implode(", ", $cnn::getAttributeList()) . ")";
 						$tables = array_merge($tables, $cnn::create_table());
+					}
 				}
-			} elseif(is_string($cn))
+			} elseif(is_string($cn)) {
+				$tableAttrs[] = $cn . "(" . implode(", ", $cn::getAttributeList()) . ")";
 				$tables = array_merge($tables, $cn::create_table());
+			}
 		}
-	} elseif(is_string($c))
+	} elseif(is_string($c)) {
+		$tableAttrs[] = $c . "(" . implode(", ", $c::getAttributeList()) . ")";
 		$tables = array_merge($tables, $c::create_table());
+	}
 }
+echo(implode("\n", $tableAttrs));
+echo("\n\n");
 echo(implode("\n\n", $tables));
 
 ?>
