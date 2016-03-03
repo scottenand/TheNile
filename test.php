ee859fb7646370b9c8a4919b7277b8dfda6cc987
scottenand <sae5201@psu.edu>
2016-03-03 10:15:27 -0500

    Update test.php

--- test.php
+++ test.php
@@ -1,11 +1,6 @@
 <?php
 
 // Will put this on a separate configuration file but for now and testing purposes it will be here.
-$servername = "localhost";
-$usernameDB = "root";
-$passwordDB = "theNilePassword";
-$dbname = "TheNile";
-
 $name = addslashes($_POST["name"]);
 $email = $_POST["email"];
 $username = $_POST["username"];
@@ -14,25 +9,15 @@ $income = $_POST["income"];
 
 
 // Create connection
-$conn = mysqli_connect($servername, $usernameDB, $passwordDB, $dbname);
-// Check connection
-
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+$conn = new database();
 
 $sql = "INSERT INTO User (name, email, username,password,income)
 VALUES ('".$name."','".$email."','".$username."','".$password."','".$income."')";
 
-if (mysqli_query($conn, $sql)) {
+if ($conn->query($sql)) {
     echo "New record created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
-
-mysqli_close($conn);
-
-
-
 
  ?>
