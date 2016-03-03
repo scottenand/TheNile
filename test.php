933bdb13c34040e719781e71f8e799ac3ff94b59
scottenand <sae5201@psu.edu>
2016-03-03 17:28:54 -0500

    Update test.php

--- test.php
+++ test.php
@@ -1,6 +1,14 @@
 <?php
 
 // Will put this on a separate configuration file but for now and testing purposes it will be here.
+
+/**
+ * include("Header.php");
+ * $arr = array("name"=>$_POST['name'], ...);
+ * $u = new User($arr);
+ * 
+ */
+
 $name = addslashes($_POST["name"]);
 $email = $_POST["email"];
 $username = $_POST["username"];
